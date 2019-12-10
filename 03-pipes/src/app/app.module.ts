210e2c068d661d1b2c2516966da1fe6b0b1eb68c
Miguelangel <miguelangelmedina2494@gmail.com>
2019-12-10 11:59:13 -0400

    03-pipe

---
+++ 03-pipes/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import {LOCALE_ID, NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import localeEs from '@angular/common/locales/es';
+import { registerLocaleData } from '@angular/common';
+import { DomseguroPipe } from './pipes/domseguro.pipe';
+registerLocaleData(localeEs);
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DomseguroPipe
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'es' }
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
